24bba834b640b509f984851287d29e498e379475
Sidharth S Shaji <111173997+Sidharth189@users.noreply.github.com>
2024-10-15 14:50:30 +0530

    updated delete function

--- C Programs/linkedlist.c
+++ C Programs/linkedlist.c
@@ -3,60 +3,119 @@
 
 struct node
 {
-    int data;
-    struct node *link;
+	int data;
+	struct node *link;
 };
 
-void insertion(struct node *start)
+void insertion(struct node **start,int d)
 {
-    int num;
-    printf("Enter the data: ");
-    scanf("%d",&num);
-    struct node *temp,*t;
-    temp=malloc(sizeof(struct node));
-    temp->data=num;
-    temp->link=NULL;
-    if(start==NULL)
-    {
-        start=temp;
-    }
-    else{
-        t=start;
-        while(t->link!=NULL)
-        {
-            t=t->link;
-        }
-        t->link=temp;
-    }
+	struct node *temp,*q;
+	q=*start;
+	temp=malloc(sizeof(struct node));
+	if(temp!=NULL)
+	{
+		temp->data=d;
+		temp->link=NULL;
+		if(*start==NULL)
+		{
+			*start=temp;
+		}
+		else{
+			while(q->link!=NULL)
+			{
+				q=q->link;
+			}
+			q->link=temp;
+		}
+	}
+	else{
+		printf("Memory allocation failed!!");
+	}
 }
 
 void display(struct node *start)
 {
-    struct node *t=start;
-    while(t->link!=NULL)
-    {
-        printf("%d",t->data);
-        t=t->link;
-    }
-
+	struct node *q=start;
+	if(start==NULL)
+	{
+		printf("\n No data");
+	}
+	else{
+		printf("\n Linked list: ");
+		while(q!=NULL)
+		{
+			printf("%d ->",q->data);
+			q=q->link;
+		}
+		printf("NULL");
+	}
 }
 
+void delete(struct node **start,int k)
+{
+	struct node *next,*before;
+	next=*start;
+	if(next==NULL)
+	{
+		printf("\n Empty List ");
+	}
+	if(next->data==k)
+	{
+		*start=next->link;
+		printf("\n Deleted !!");
+		free(next);
+	}
+	else{
+		while(next!=NULL)
+		{
+			if(next->data==k)
+			{
+				before->link=next->link;
+				printf("\n Deleted !!");
+				free(next);
+				break;
+			}
+			else{
+				before=next;
+				next=next->link;
+			}
+		}
+	}
+}
+			
 void main()
 {
-    struct node *start;
-    start=NULL;
-    int ch=1;
-    do{
-        printf("1.Insertion \n2.Deletion \n3.display");
-        switch(ch)
-        {
-            case 1: insertion(start);
-                    break;
-        
-            case 2: break;
-
-            case 3: display(start);
-                    break;
-        }
-    }while(ch!=0);
+	int ch,data;
+	struct node *start,*q;
+	start=NULL;
+	do{
+		printf("\n1.Insertion\t\t2.Display\t\t3.Delete\t\t0.exit");
+		printf("\n Enter your choice: ");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:printf("\n Enter the data :");
+				scanf("%d",&data);
+				insertion(&start,data);
+				break;
+		
+			case 2:display(start);
+				break;
+				
+			case 3:printf("\n Enter the data to delete: ");
+				scanf("%d",&data);
+				delete(&start,data);
+				break;
+		
+			default:printf("\n wrong option");
+		}
+	}while(ch!=0);
+	
+	q=start;
+	while(q!=NULL)
+	{
+		q=q->link;
+		free(q);
+	}
+	free(start);
 }
