c1e24140a3f8edc53b12943f0e5dffd95cd22a5d
warrior189 <sidhathsshaji189@gmail.com>
2024-10-16 18:42:58 +0530

    multibase pattern

---
+++ Python Programs/pattern/multibase1.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the limit: "))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    for j in range(n-i+1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print("")
