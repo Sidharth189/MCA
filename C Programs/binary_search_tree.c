d780a2ca5bef97cecf17d47a8ebf3e92ab0ba518
warrior189 <sidhathsshaji189@gmail.com>
2024-11-10 22:55:12 +0530

    deletion and search operation

--- C Programs/binary_search_tree.c
+++ C Programs/binary_search_tree.c
@@ -7,8 +7,6 @@ struct node{
     struct node* right;
 };
 
-struct node
-
 struct node* create_node(int data)
 {
     struct node* temp=malloc(sizeof(struct node));
@@ -60,7 +58,7 @@ void pre_order(struct node* root)
     {
         return;
     }
-    printf("%d",root->data);
+    printf("%d->",root->data);
     pre_order(root->left);
     pre_order(root->right);
 }
@@ -73,7 +71,7 @@ void post_order(struct node* root)
     }
     post_order(root->left);
     post_order(root->right);
-    printf("%d",root->data);
+    printf("%d->",root->data);
 }
 
 void in_order(struct node* root)
@@ -87,13 +85,112 @@ void in_order(struct node* root)
     in_order(root->right);
 
 }
-struct node in_order_queue(struct node* root)
-{
 
-}
-void delete()
+struct node* find_inorder_successor(struct node* q)
 {
-    
+    while(q!=NULL && q->left!=NULL)
+    {
+        q=q->left;
+    }
+    return q;
+}
+
+void delete(struct node** root,int key)
+{
+    struct node* q=*root,*temp,*before=NULL;
+
+    if(*root==NULL)
+    {
+        printf("\n Empty tree !!");
+        return;
+    }
+            //traversal upto node
+    while(q!=NULL && q->data!=key)
+    {
+        before=q;
+        if(key<q->data)
+        {   
+            q=q->left;
+        }
+        else
+        {
+            q=q->right;
+        }
+    }
+        //if no child
+    if(q->left==NULL && q->right==NULL)
+    {
+        if(before==NULL)
+        {
+            free(q);
+            *root=NULL;
+        }
+        else if(before->left ==q)
+        {
+            before->left=NULL;
+            free(q);
+        }
+        else{
+            before->right=NULL;
+            free(q);
+        }
+    }
+        //one child
+    else if(q->left==NULL)
+    {
+        if(before==NULL)
+        {
+            *root=q->right;
+        }
+        before=q->right;
+        free(q);
+    }
+    else if(q->right==NULL)
+    {
+        if(before==NULL)
+        {
+            *root=q->left;
+        }
+        before=q->left;
+        free(q);
+    }
+    else{
+        temp=find_inorder_successor(q->right);
+        q->data=temp->data;
+        delete(&(q->right),temp->data);
+    }    
+}
+
+void search(struct node* root,int key)
+{
+    struct node* q=root;
+    if(root==NULL)
+    {
+        printf("Empty tree !!");
+        return;
+    }
+    while(q!=NULL)
+    {
+        if(q->data==key)
+        {
+            printf("\n Search successfull");
+            printf("\n %d found !!",q->data);
+            return;
+        }
+        else if(q->data<key)
+        {
+            q=q->right;
+        }
+        else{
+            q=q->left;
+        }
+    }
+
+    if(q==NULL)
+    {
+        printf("\n Key not found !!");
+    }
+
 }
 
 void main()
@@ -101,7 +198,7 @@ void main()
     int ch,key;
     struct node* root=NULL;
     do{
-        printf("\n1.Insertion\t\t2.Deletion\t\t3.In-order\n4.Pre-order\t\t5.Post-order\t\t0.Exit");
+        printf("\n1.Insertion\t\t2.Deletion\t\t3.In-order\n4.Pre-order\t\t5.Post-order\t\t6.Search\t\t0.Exit");
         printf("\n Enter  your choice: ");
         scanf("%d",&ch);
 
@@ -114,6 +211,8 @@ void main()
 
             case 2:printf("\nEnter the data to delete: ");
                 scanf("%d",&key);
+                delete(&root,key);
+                printf("\n Deleted !!");
                 break;
 
             case 3:in_order(root);
@@ -125,7 +224,12 @@ void main()
             case 5:post_order(root);
                 break;
 
-            case 0:break;
+            case 6:printf("Enter the data to be searched: ");
+                scanf("%d",&key);
+                search(root,key);
+                break;
+
+            case 0:exit(0);break;
 
             default:printf("\n Invalid choice !!");
         }
